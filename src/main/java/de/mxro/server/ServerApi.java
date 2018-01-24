4976131ca24def7992426faba8566019ff898e00
Max Rohde <mxro@nowhere.com>
2018-01-25 10:18:48 +1100

    Allowing for saver shutdowns.

--- src/main/java/de/mxro/server/ServerApi.java
+++ src/main/java/de/mxro/server/ServerApi.java
@@ -1,17 +1,25 @@
 package de.mxro.server;
 
-import delight.factories.FactoryCollection;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import de.mxro.server.internal.DefaultComponentManager;
 import de.mxro.server.management.ComponentManager;
 import de.mxro.service.callbacks.ShutdownCallback;
+import delight.async.AsyncCommon;
+import delight.async.Operation;
+import delight.async.callbacks.SimpleCallback;
+import delight.async.callbacks.ValueCallback;
+import delight.concurrency.Concurrency;
+import delight.concurrency.Concurrent;
+import delight.concurrency.wrappers.SimpleAtomicBoolean;
+import delight.concurrency.wrappers.SimpleTimer;
+import delight.factories.FactoryCollection;
+import delight.functional.Success;
+import delight.simplelog.Log;
 
 public class ServerApi {
 
-    private final static boolean ENABLE_LOG = false;
 
     /**
      * <p>
@@ -29,41 +37,72 @@ public class ServerApi {
 
     }
 
-    public static void performShutdown(final List<ServerComponent> toShutdown, final ShutdownCallback callback) {
+    public static void performShutdown(final List<ServerComponent> toShutdown, final int timeout, final Concurrency concurrency, final ShutdownCallback callback) {
 
-        if (toShutdown.size() == 0) {
-            callback.onSuccess();
-            if (ENABLE_LOG) {
-                System.out.println(ServerApi.class + ": Shutdown complete.");
-            }
-            return;
-        }
+    	List<Operation<Success>> list = new ArrayList<Operation<Success>>(toShutdown.size());
+    	
+    	for (final ServerComponent c: toShutdown) {
+    		list.add(new Operation<Success>() {
+				
+				@Override
+				public void apply(final ValueCallback<Success> callback) {
+					
+					final SimpleAtomicBoolean failed = concurrency.newAtomicBoolean(false);
+					final SimpleAtomicBoolean succeeded = concurrency.newAtomicBoolean(false);
+					
+					final SimpleTimer timer = concurrency.newTimer().scheduleOnce(timeout, new Runnable() {
+						
+						@Override
+						public void run() {
+							if (succeeded.get()) {
+								return;
+							}
+							failed.set(true);
+							callback.onFailure(new Exception("Component not shut down in timeout: "+c));
+						}
+					});
+					
+					c.stop(new SimpleCallback() {
+						
+						@Override
+						public void onFailure(Throwable t) {
+							if (failed.get()) {
+								Log.warn("Component shutdown failed. Excpetion reported: "+t.getMessage(), t);
+								return;
+							}
+							succeeded.set(true);
+							timer.stop();
+						}
+						
+						@Override
+						public void onSuccess() {
+							if (failed.get()) {
+								return;
+							}
+							succeeded.set(true);
+							timer.stop();
+							callback.onSuccess(Success.INSTANCE);
+							
+						}
+					});
+				}
+			});
+    	}
+    	
+    	Concurrent.sequential(list, concurrency, new ValueCallback<List<Success>>() {
 
-        final ServerComponent server = toShutdown.get(0);
+			@Override
+			public void onFailure(Throwable t) {
+				callback.onFailure(t);
+			}
 
-        if (ENABLE_LOG) {
-            System.out.println(ServerApi.class + ": Shutting down " + server);
-        }
-        toShutdown.remove(0);
-
-        final List<ServerComponent> remainingServers = new ArrayList<ServerComponent>(toShutdown);
-        server.stop(new ShutdownCallback() {
-
-            @Override
-            public void onSuccess() {
-                if (ENABLE_LOG) {
-                    System.out.println(ServerApi.class + ": Shut  down " + server);
-                }
-                performShutdown(remainingServers, callback);
-            }
-
-            @Override
-            public void onFailure(final Throwable t) {
-                t.printStackTrace();
-                performShutdown(remainingServers, callback);
-                callback.onFailure(t);
-            }
-        });
+			@Override
+			public void onSuccess(List<Success> value) {
+				callback.onSuccess();
+			}
+		});
+    	
+        
 
     }
 
